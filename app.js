5d34f5d7b30dff2f5b8d221bf13910f360aa0102
Kojo <kojoaddo598@gmail.como>
2025-11-05 13:10:42 +0000

    final

--- app.js
+++ app.js
@@ -2,20 +2,16 @@ const navMenuEl = document.querySelector('.nav--menu')
 const hamburgerEl = document.querySelector('.hamburger')
 const mainEl = document.querySelector('.main')
 
-
 hamburgerEl.addEventListener('click',()=>{
     navMenuEl.classList.toggle("nav--open")
     hamburgerEl.classList.toggle("hamburger--cross")
-
 })
 
 navMenuEl.addEventListener('click',()=>{
     navMenuEl.classList.remove("nav--open")
     hamburgerEl.classList.remove("hamburger--cross")
-
 })
 
 mainEl.addEventListener('mousemove',()=>{
-    navMenuEl.classList.remove("nav--open")
-    
+    navMenuEl.classList.remove("nav--open")  
 })
