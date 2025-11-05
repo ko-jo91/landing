a1a64ccc394f2f2e42fb088f08636cdfac2331eb
Kojo <kojoaddo598@gmail.como>
2025-11-05 12:57:29 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const navMenuEl = document.querySelector('.nav--menu')
+const hamburgerEl = document.querySelector('.hamburger')
+const mainEl = document.querySelector('.main')
+
+
+hamburgerEl.addEventListener('click',()=>{
+    navMenuEl.classList.toggle("nav--open")
+    hamburgerEl.classList.toggle("hamburger--cross")
+
+})
+
+navMenuEl.addEventListener('click',()=>{
+    navMenuEl.classList.remove("nav--open")
+    hamburgerEl.classList.remove("hamburger--cross")
+
+})
+
+mainEl.addEventListener('mousemove',()=>{
+    navMenuEl.classList.remove("nav--open")
+    
+})
